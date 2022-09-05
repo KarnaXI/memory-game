26583fad85991071145731223cb8fe9524aa8b6d
Asam Afzal <aainvests@outlook.com>
2022-09-05 16:26:14 -0400

    final v1

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ function App() {
   function handleClick(id){
     if(clickedCharacters.includes(id)){
       if(score > bestScore){
-        setScore(0)
+        setBestScore(bestScore=score)
+        setScore(0);
       }
       else{
         setScore(0);
       }
+      setClickedCharacters([]);
     }
     else{
       setScore(score+1);
-      setBestScore(bestScore+1);
       setClickedCharacters(([...clickedCharacters, id]));
-      console.log(clickedCharacters);
     }
   }
 
@@ -30,18 +30,21 @@ function App() {
       <header className="App-header">
         <div className='header-title'>
           <h1 className="site-title">Memory Game</h1>
-          <div className="score-container">
-            <p><strong>Score:</strong> {score}</p>
-            <p><strong>Best Score:</strong> {score}</p>
+          <div className='instructions-scores'>
+            <div className='instructions'>
+              <p>Welcome, click on any character to begin. Be careful not to click the same character twice!</p>
             </div>
+            <div className="score-container">
+              <p><strong>Score:</strong> {score}</p>
+              <p><strong>Best Score:</strong> {bestScore}</p>
+            </div>
+          </div>
+
         </div>
-        
-        <button onClick={() => setScore(score + 1)}>Click me</button>
-        <div className='cards-container'>
+      </header>
+      <div className='cards-container'>
           {randomizeCharacters().map((character) => <Cards key={character.id} character={character} handleClick={handleClick}/>)}
         </div>
-
-      </header>
     </div>
   );
 }
